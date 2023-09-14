6d7efa8452ab2e31cb6c5e5f2796b658a4ddac12
Elisabeth Theoret <101665871+elisabeththeoret@users.noreply.github.com>
2023-09-14 13:26:00 -0400

    Changer le nom de l'app (title)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'vinstock-angular'`, () => {
+  it(`should have as title 'VinStock'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('vinstock-angular');
+    expect(app.title).toEqual('VinStock');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('vinstock-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('VinStock app is running!');
   });
 });
